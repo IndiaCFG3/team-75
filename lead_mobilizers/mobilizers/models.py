7b76111128022909f8d033c7cc0c5f3142ddddaf
Vishal <vishalgulati2k@gmail.com>
2020-08-16 12:49:43 +0530

    Learner Form

--- lead_mobilizers/mobilizers/models.py
+++ lead_mobilizers/mobilizers/models.py
@@ -12,6 +12,10 @@ class City(models.Model):
 class Course(models.Model):
     Name=models.CharField(max_length=30)
     city=models.ForeignKey(City,on_delete=models.CASCADE)
+class Mobilizer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    contactnumber=models.CharField(max_length=13)
 class Learners(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -19,12 +23,9 @@ class Learners(models.Model):
     course=models.ForeignKey(Course,on_delete=models.CASCADE)
     city=models.ForeignKey(City,on_delete=models.CASCADE)
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    age=models.IntegerField(max_length=3)
+    age=models.IntegerField()
     gender=models.ForeignKey(Gender,on_delete=models.CASCADE)
     status=models.ForeignKey(Status,on_delete=models.CASCADE)
     mobilizer=models.ForeignKey(Mobilizer,on_delete=models.CASCADE)
-class Mobilizer(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    contactnumber=models.CharField(max_length=13)
+
     
