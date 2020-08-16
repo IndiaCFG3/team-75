25caf15ec915088ade8ae41032f55543ccebb943
Vishal <vishalgulati2k@gmail.com>
2020-08-16 16:40:21 +0530

    integrated

--- lead_mobilizers/lead_mobilizers/urls.py
+++ lead_mobilizers/lead_mobilizers/urls.py
@@ -22,6 +22,6 @@ from mobilizers import views as mobilizers_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',lead_mobilizers_views.home,name="index"),
-    path('login/', auth_views.LoginView.as_view(template_name='mobilizers/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='mobilizers/index.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='mobilizers/logout.html'), name='logout')
 ]
