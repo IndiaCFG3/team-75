35b1adbf96a6aeaa03822022c41201f44d14d7b9
Vishal <vishalgulati2k@gmail.com>
2020-08-16 18:18:30 +0530

    Updated urls

--- lead_mobilizers/lead_mobilizers/settings.py
+++ lead_mobilizers/lead_mobilizers/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'lead_mobilizers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'/mobilizers/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
