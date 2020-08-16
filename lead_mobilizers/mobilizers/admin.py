59eb93a8d2daca86004275f6e89dc5260e194e93
Vishal <vishalgulati2k@gmail.com>
2020-08-16 14:27:19 +0530

    Update

--- lead_mobilizers/mobilizers/admin.py
+++ lead_mobilizers/mobilizers/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Status,Gender,Category,City,Course,Mobilizers
+from .models import Status,Gender,Category,City,Course,Mobilizers,Learners
 admin.site.register(Status)
 admin.site.register(Gender)
 admin.site.register(Category)
 admin.site.register(City)
 admin.site.register(Course)
 admin.site.register(Mobilizers)
+admin.site.register(Learners)
